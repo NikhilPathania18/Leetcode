a77427581a16fa031a769dd12864d374da22e65c
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-06-07 22:04:47 +0530

    Time: 3 ms (72.68%), Space: 9.2 MB (30.68%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> nums3;
+        int i=0,j=0;
+        while(i<m||j<n)
+        {
+            if(i!=m&&j!=n)
+            {
+                if(nums1[i]<nums2[j])
+                {
+                    nums3.push_back(nums1[i]);
+                    i++;
+                }
+                else
+                {
+                    nums3.push_back(nums2[j]);
+                    j++;
+                }
+            }
+            else if(i<m&&j==n)
+            {
+                nums3.push_back(nums1[i]);
+                i++;
+            }
+            else
+            {
+                nums3.push_back(nums2[j]);
+                j++;
+            }
+        }
+        nums1=nums3;
+    }
+};
