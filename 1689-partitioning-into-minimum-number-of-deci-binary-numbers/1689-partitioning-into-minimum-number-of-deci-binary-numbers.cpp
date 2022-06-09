50f916aa3a229e95beb2daede191302985b6804a
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-06-09 13:32:40 +0530

    Time: 38 ms (58.24%), Space: 13.6 MB (18.23%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -4,10 +4,11 @@ public:
         char a=n[0];
         for(int i=0;i<n.size();i++)
         {
-            if(n[i]>a)
-                a=n[i];
+            
             if(a=='9')
                 return 9;
+            if(n[i]>a)
+                a=n[i];
         }
         return a-'0';
     }
