7625f28ddb16b8786a78284690187e26a3089a10
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-06-13 00:26:17 +0530

    Time: 382 ms (33.26%), Space: 144.7 MB (73.15%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -12,42 +12,26 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        // if(root==NULL)
-        //     return 0;
-        // queue<TreeNode*> q;
-        // int level=1;
-        // q.push(root);
-        // while(!q.empty())
-        // {
-        //     int size=q.size();
-        //     for(int i=0;i<size;i++)
-        //     {
-        //         if(!q.front()->left&&!q.front()->right)
-        //             return level;
-        //         if(q.front()->left)
-        //             q.push(q.front()->left);
-        //         if(q.front()->right)
-        //             q.push(q.front()->right);
-        //         q.pop();
-        //     }
-        //     level++;
-        // }
-        // return level;
-        
         if(root==NULL)
             return 0;
-        
-        if(!root->left && !root->right)
-            return 1;
-        
-        int l,r;
-        if(root->left)
-         l = minDepth(root->left)+1;
-        
-        if(root->right)
-         r = minDepth(root->right)+1;
-        
-        if(l<r) return l;
-        else return r;
+        queue<TreeNode*> q;
+        int level=1;
+        q.push(root);
+        while(!q.empty())
+        {
+            int size=q.size();
+            for(int i=0;i<size;i++)
+            {
+                if(!q.front()->left&&!q.front()->right)
+                    return level;
+                if(q.front()->left)
+                    q.push(q.front()->left);
+                if(q.front()->right)
+                    q.push(q.front()->right);
+                q.pop();
+            }
+            level++;
+        }
+        return level;
     }
 };
