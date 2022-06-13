d150ce715db4802975b9de0fa1aa3ca425576ec3
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-06-13 09:42:25 +0530

    Time: 13 ms (97.19%), Space: 16 MB (80.05%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -11,65 +11,32 @@
  */
 class Solution {
 public:
-//     int count=0;
-//     void bst(TreeNode* root,int sum)
-//     {
-//         if(root==NULL)
-//             return;
-//         sum-=root->val;
-//         if(sum==0)
-//             count++;
-//         bst(root->left,sum);
-//         bst(root->right,sum);
+    int count=0;
+    void bst(TreeNode* root,unsigned long long sum)
+    {
+        if(root==NULL)
+            return;
+        sum-=root->val;
+        if(sum==0)
+            count++;
+        bst(root->left,sum);
+        bst(root->right,sum);
         
-//     }
-//     void bst2(TreeNode* root,int sum)
-//     {
-//         if(root==NULL)
-//             return ;
-//         bst(root,sum);
-//         bst(root->left,sum);
-//         bst(root->right,sum);
-//     }
-//     int pathSum(TreeNode* root, int targetSum) {
-//         if(root==NULL)
-//             return 0;
-        
-//         bst2(root,targetSum);
-//         return count;
-//     }
-    int ans;
-    void solve(TreeNode* root,  unsigned long long sum){
-           if(root==NULL){
-                   return;
-           }
-           sum-=root->val;
-           if(sum==0){
-                   ans++;
-           }
-           solve(root->left,sum);
-           solve(root->right,sum);
     }
-    void helper(TreeNode* root, unsigned long long sum){
-           
-            solve(root,sum);
-            if(root->left){
-                     helper(root->left,sum);
-            }
-            if(root->right){
-                    helper(root->right,sum);
-            }
+    void bst2(TreeNode* root,int sum)
+    {
+        if(root==NULL)
+            return ;
+        bst(root,sum);
+        bst2(root->left,sum);
+        bst2(root->right,sum);
     }
     int pathSum(TreeNode* root, int targetSum) {
-            ans=0;
-            if(root==NULL){
-                    return 0;
-            }
-            unsigned long long sum=targetSum;
-            helper(root,sum);
-            return ans;
-            
+        if(root==NULL)
+            return 0;
         
+        bst2(root,targetSum);
+        return count;
     }
 };
 
