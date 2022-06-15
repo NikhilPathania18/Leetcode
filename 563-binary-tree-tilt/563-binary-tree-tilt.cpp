7275f23af59e9fa33093e673231b7008d4ec7e0c
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-06-15 23:58:22 +0530

    Time: 62 ms (5.32%), Space: 23.7 MB (37.97%) - LeetHub

---
+++ 563-binary-tree-tilt/563-binary-tree-tilt.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum=0;
+    int getSum(TreeNode* root)
+    {
+        if(root==NULL)
+            return 0;
+        return (getSum(root->left)+getSum(root->right)+root->val);
+    }
+    void makeTilt(TreeNode* root) {
+        if(root==NULL)
+            return;
+        root->val=abs(getSum(root->left)-getSum(root->right));
+        makeTilt(root->left);
+        makeTilt(root->right);
+    }
+    void inorder(TreeNode* root)
+    {
+        if(root==NULL)
+            return;
+        inorder(root->left);
+        sum+=root->val;
+        inorder(root->right);
+    }
+    int findTilt(TreeNode* root)
+    {
+        if(root==NULL)
+            return 0;
+        makeTilt(root);
+        inorder(root);
+        return sum;
+    }
+};
