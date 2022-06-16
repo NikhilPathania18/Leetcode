74787748d2b5ac1ff3a7fc9c6eefd48618ff09d1
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-06-16 13:03:41 +0530

    Time: 44 ms (69.90%), Space: 11 MB (58.93%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,66 +1,16 @@
 
 class Solution {
-public:
-    string func(string s)
-    {
-        
-        if(s=="")
-            return "";
-        if(s.size()==1)
-            return s;
-        int i,j,k=0,len=1;
-        string str;
-        while(k<s.size())
-        {
-            i=k-1;
-            j=k+1;
-            while(i>=0&&j<s.size())
-            {
-                if(s[i]!=s[j])
-                    break;
-                i--;
-                j++;
-            }
-            if(j-i+1>len)
-            {
-                len=j-i+1;
-                str="";
-                str=s.substr(i+1,j-i+1);
-            }
-            i=k-1;
-            j=k;
-            while(i>=0&&j<s.size())
-            {
-                if(s[i]!=s[j])
-                    break;
-                i--;
-                j++;
-            }
-            if(j-i+1>len)
-            {
-                str="";
-                str=s.substr(i+1,j-i+1);
-                len=j-i+1;
-            }
-            k++;
-        }
-        return str;
-    }
-    
-    
-    
-    string longestPalindrome(string s) {
+public: string longestPalindrome(string s) {
         int i,j,k=0;
         int maxL=1;
+        int currLen;
         string largest;
         largest.push_back(s[0]);
-        int size=s.size();
-        int currLen;
-        for(int k=0;k<size;k++)
+        for(k;k<s.size();k++)
         {
             i=k-1;
             j=k+1;
-            while((i>=0&&j<size)&&(s[i]==s[j]))
+            while((i>=0&&j<s.size())&&(s[i]==s[j]))
             {
                 i--;
                 j++;
@@ -71,12 +21,10 @@ public:
                 maxL=currLen;
                 largest=s.substr(i+1,currLen);
             }
-        }
-        for(int k=0;k<size;k++)
-        {
+            //-----------> Second case
             i=k-1;
             j=k;
-            while((i>=0&&j<size)&&(s[i]==s[j]))
+            while((i>=0&&j<s.size())&&(s[i]==s[j]))
             {
                 i--;
                 j++;
