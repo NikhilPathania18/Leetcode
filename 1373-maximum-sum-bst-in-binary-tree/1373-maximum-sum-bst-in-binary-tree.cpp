fa7e28fa13fa23ebd2a19dc6998cadfc0640e6ed
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-06-18 22:17:53 +0530

    Time: 324 ms (33.48%), Space: 105.2 MB (57.22%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -17,15 +17,12 @@ public:
         int sum=0;
         int minV=INT_MAX,maxV=INT_MIN;
     };
-    
-    
     node getSum(TreeNode* root)
     {
         node obj;
         if(root==NULL)
-        {
             return obj;
-        }
+        
         node oL=getSum(root->left);
         node oR=getSum(root->right);
         
@@ -36,65 +33,13 @@ public:
             obj.maxV=INT_MAX;
             return obj;
         }
-        
-        int sum=oL.sum+oR.sum+root->val;
+        int sum=root->val+oL.sum+oR.sum;
         maxSum=max(maxSum,sum);
-        
         obj.sum=sum;
-        obj.minV=min(oL.minV,root->val);
-        obj.maxV=max(oR.maxV,root->val);
+        obj.minV=min(root->val,oL.minV);
+        obj.maxV=max(root->val,oR.maxV);
         return obj;
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    node sumFunc(TreeNode* root)
-    {
-        node obj;
-        if(root==NULL)
-        {
-            return obj;
-        }
-        node objLeft=sumFunc(root->left);
-        node objRight=sumFunc(root->right);
-        if(root->val<=objLeft.maxV||root->val>=objRight.minV)
-        {
-            cout<<root->val;
-            obj.minV=objLeft.maxV;
-            obj.maxV=objRight.minV;
-            return obj;
-        }
-        maxSum=max(maxSum,objLeft.sum+objRight.sum+root->val);
-        // cout<<objLeft.sum+objRight.sum+root->val<<"\n";
-        obj.sum=objLeft.sum+objRight.sum+root->val;
-        obj.minV=min(objLeft.minV,root->val);
-        obj.maxV=max(objRight.maxV,root->val);
-        return obj;
-    }
-    
     int maxSumBST(TreeNode* root) {
         getSum(root);
         return maxSum;
