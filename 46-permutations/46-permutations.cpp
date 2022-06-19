e97167f44d4f9078a679ab71d423b49a67d82af0
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-06-19 20:03:05 +0530

    Time: 4 ms (63.99%), Space: 7.7 MB (78.42%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,30 +1,46 @@
 class Solution {
 public:
-    void perm(vector<int>& nums,vector<int>& arr,int index,vector<bool> &isTaken,vector<vector<int>> &ans)
+//     void perm(vector<int>& nums,vector<int>& arr,int index,vector<bool> &isTaken,vector<vector<int>> &ans)
+//     {
+//         if(arr.size()==nums.size())
+//         {
+//             ans.push_back(arr);
+//             return;
+//         }
+        
+//         for(int j=0;j<nums.size();j++)
+//             {
+//                 if(isTaken[j])
+//                     continue;
+                
+//                 arr.push_back(nums[j]);
+//                 isTaken[j]=true;
+//                 perm(nums,arr,j+1,isTaken,ans);
+//                 isTaken[j]=false;
+//                 arr.pop_back();
+//             }
+//     }
+    
+    void perm2(vector<int>& nums,int index,vector<vector<int>> &ans)
     {
-        if(arr.size()==nums.size())
+        if(index==nums.size())
         {
-            ans.push_back(arr);
+            ans.push_back(nums);
             return;
         }
-        
-        for(int j=0;j<nums.size();j++)
-            {
-                if(isTaken[j])
-                    continue;
-                
-                arr.push_back(nums[j]);
-                isTaken[j]=true;
-                perm(nums,arr,j+1,isTaken,ans);
-                isTaken[j]=false;
-                arr.pop_back();
-            }
+        for(int i=index;i<nums.size();i++)
+        {
+            swap(nums[index],nums[i]);
+            perm2(nums,index+1,ans);
+            swap(nums[index],nums[i]);
+        }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<int> arr;
         vector<vector<int>> ans;
         vector<bool> isTaken(nums.size(),false);
-        perm(nums,arr,0,isTaken,ans);
+        // perm(nums,arr,0,isTaken,ans);
+        perm2(nums,0,ans);
         return ans;
     }
 };
