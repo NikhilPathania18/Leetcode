5e53e03cd8a665efffa22b1c9e46f05cb5e1c1a4
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-06-22 11:09:39 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (15.96%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
-    string solve(vector<char>& nums, string &arr, int index,int n,int k,vector<bool> &isTaken,vector<string>& ans)
+    int fact(int n)
     {
-    
-        if(arr.size()==n)
-        {
-              ans.push_back(arr) ;
-                if(ans.size()==k)
-                    return ans[ans.size()-1];
-                return "";
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(isTaken[i])
-                continue;
-            
-            arr.push_back(nums[i]);
-            isTaken[i]=true;
-            string str=solve(nums,arr,index+1,n,k,isTaken,ans);
-            if(str!="")
-                return str;
-            isTaken[i]=false;
-            arr.pop_back();
-        }
-        return "";
+        if(n<=1)
+            return 1;
+        return n*fact(n-1);
+    }
+    string solve(string str,vector<char> &nums,int n,int k)
+    {
+        if(n==0)
+            return str;
+        int total=fact(n);
+        int perSet=total/n;
+        
+        int whichSet=(k)/perSet;
+        int whatNo=(k)%perSet;
+        
+        str.push_back(nums[whichSet]);
+        nums.erase(nums.begin()+whichSet);
+        return solve(str,nums,n-1,whatNo);
     }
     string getPermutation(int n, int k) {
+        string str="";
         vector<char> nums;
         for(int i=0;i<n;i++)
+        {
             nums.push_back('0'+i+1);
-        vector<bool> isTaken(n,false);
-        vector<string> ans;
-        string arr="";
-        return solve(nums,arr,0,n,k,isTaken,ans);
-        
+        }
+        return solve(str,nums,n,k-1);
     }
 };
