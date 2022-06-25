9a36fa5b8aa8d3b1eee0fe9a8a8ba4f141897587
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-06-25 15:58:03 +0530

    Time: 31 ms (17.30%), Space: 10.3 MB (48.19%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,20 +1,5 @@
 class Solution {
 public:
-    int path(vector<vector<int>>& grid,int row,int col,int m,int n,vector<vector<int>>& dp)
-    {
-        if(row==m-1)
-            return dp[row][col]=grid[row][col];
-        if(dp[row][col]!=-1)
-            return dp[row][col];
-        int left,bottom,right;
-        left=bottom=right=INT_MAX;
-        if(col>0)
-            left=path(grid,row+1,col-1,m,n,dp);
-        if(col<n-1)
-            right=path(grid,row+1,col+1,m,n,dp);
-        bottom=path(grid,row+1,col,m,n,dp);
-        return dp[row][col]=min(left,min(right,bottom))+grid[row][col];
-    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int m=matrix.size(),n=matrix[0].size();
         vector<vector<int>> dp(m,vector<int>(n,-1));
@@ -40,6 +25,5 @@ public:
         for(int i=0;i<n;i++)
             ans=min(ans,dp[0][i]);
         return ans;
-        
     }
 };
