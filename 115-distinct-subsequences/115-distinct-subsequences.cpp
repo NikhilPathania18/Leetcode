b7627ac11fc3b659a736ef54ed197668d1e82ef0
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-06-29 01:18:58 +0530

    Time: 44 ms (70.44%), Space: 30.2 MB (16.58%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -20,9 +20,9 @@ public:
         vector<double> prev(n+1,0);
         vector<double> curr(n+1,0);
        
+        prev[0]=curr[0]=1;
         for(int i=1;i<=m;i++)
         {
-            prev[0]=1;
             for(int j=1;j<=n;j++)
             {
                 if(s[i-1]==t[j-1])
