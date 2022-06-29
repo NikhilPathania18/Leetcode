9a61cb7bd79be088f6b6379433e254fa12b5b748
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-06-30 00:12:25 +0530

    Time: 255 ms (13.47%), Space: 14.4 MB (20.99%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -27,7 +27,42 @@ public:
     int cherryPickup(vector<vector<int>>& grid) {
         int m=grid.size(),n=grid[0].size();
         vector<vector<vector<int>>> dp(m,vector<vector<int>>(n,vector<int>(n,-1)));
-        // vector<vector<vector<int>>> dp(5,vector<int>(3,vector<int>(2,4)));
-        return solve(grid, 0,0,n-1,m,n,dp);
+        for(int c1=0;c1<n;c1++)
+        {
+            for(int c2=0;c2<n;c2++)
+            {
+                if(c1==c2)
+                    dp[m-1][c1][c2]=grid[m-1][c1];
+                else
+                    dp[m-1][c1][c2]=grid[m-1][c1]+grid[m-1][c2];
+            }
+        }
+        
+        for(int row=m-2;row>=0;row--)
+        {
+            for(int c1=0;c1<n;c1++)
+            {
+                for(int c2=0;c2<n;c2++)
+                {
+                    int value=0;
+                    if(c1==c2)
+                        value=grid[row][c1];
+                    else
+                        value=grid[row][c1]+grid[row][c2];
+                    int ans=INT_MIN;
+                    for(int i=-1;i<=1;i++)
+                    {
+                        for(int j=-1;j<=1;j++)
+                        {
+                            if(c1+i<0||c2+j<0||c1+i>=n||c2+j>=n)
+                                continue;
+                            ans=max(ans,dp[row+1][c1+i][c2+j]+value);
+                        }
+                    }
+                    dp[row][c1][c2]=ans;
+                }
+            }
+        }
+        return dp[0][0][n-1];
     }
 };
