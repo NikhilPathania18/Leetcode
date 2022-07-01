c080f7b66a93f34d9c1645e0449fccec2508099d
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-07-01 14:13:23 +0530

    Time: 11 ms (84.22%), Space: 8.9 MB (56.45%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -15,8 +15,25 @@ public:
     }
     int minDistance(string word1, string word2) {
         int m=word1.size(),n=word2.size();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        vector<vector<int>> dp(m+1,vector<int>(n+1));
+        for(int i=0;i<=n;i++)   dp[0][i]=i;
+        for(int j=0;j<=m;j++)   dp[j][0]=j;
         
-        return minDist(word1,word2,m-1,n-1,dp);
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(word1[i-1]==word2[j-1])
+                    dp[i][j]=dp[i-1][j-1];
+                else
+                {
+                    int insert=1+dp[i][j-1];
+                    int del=1+dp[i-1][j];
+                    int replace=1+dp[i-1][j-1];
+                    dp[i][j]=min(insert,min(del,replace));
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
