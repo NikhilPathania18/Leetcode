751d2287f3a5f860aa08aca30de067d309cb62d8
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-07-03 23:58:28 +0530

    Time: 8 ms (84.47%), Space: 10.9 MB (81.96%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(k+1)));
+        // vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(k+1)));
+        vector<vector<int>> temp(2,vector<int>(k+1));
+        vector<vector<int>> curr(2,vector<int>(k+1));
         for(int i=0;i<=k;i++)
-            dp[n][0][i]=dp[n][1][i]=0;
+            temp[0][i]=temp[1][i]=0;
         for(int index=n-1;index>=0;index--)
         {
             for(int hasBought=0;hasBought<=1;hasBought++)
@@ -12,14 +14,15 @@ public:
                 for(int remaining=0;remaining<=k;remaining++)
                 {
                     if(hasBought)
-                        dp[index][hasBought][remaining]=max(prices[index]+dp[index+1][0][remaining],dp[index+1][1][remaining]);
+                        curr[hasBought][remaining]=max(prices[index]+temp[0][remaining],temp[1][remaining]);
                     else if(remaining==0)
-                        dp[index][hasBought][remaining]=0;
+                        curr[hasBought][remaining]=0;
                     else
-                        dp[index][hasBought][remaining]=max(-1*prices[index]+dp[index+1][1][remaining-1],dp[index+1][0][remaining]);
+                        curr[hasBought][remaining]=max(-1*prices[index]+temp[1][remaining-1],temp[0][remaining]);
                 }
             }
+            temp=curr;
         }
-        return dp[0][0][k];
+        return temp[0][k];
     }
 };
