53b921189b05c39e225570630419a8470fb703ca
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-07-05 15:48:58 +0530

    Time: 22 ms (5.22%), Space: 13.4 MB (6.61%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -11,29 +11,47 @@
  */
 class Solution {
 public:
+    int maxLevel=0;
+    vector<int> rightView(vector<int>& ans,TreeNode* root,int currLevel)
+    {
+        if(root==NULL)
+            return {};
+        if(currLevel>maxLevel)
+        {
+            ans.push_back(root->val);
+            maxLevel=currLevel;
+        }
+        rightView(ans,root->right,currLevel+1);
+        rightView(ans,root->left,currLevel+1);
+        return ans;
+        
+    }
     vector<int> rightSideView(TreeNode* root) {
         if(root==NULL)
             return {};
-        queue<TreeNode*> q;
-        q.push(root);
-        vector<int> arr;
-        TreeNode* temp;
-        int size;
-        while(!q.empty())
-        {
-            size=q.size();
-            temp=q.front();
-            arr.push_back(temp->val);
-            for(int i=0;i<size;i++)
-            {
-                if(q.front()->right)
-                    q.push(q.front()->right);
-                if(q.front()->left)
-                    q.push(q.front()->left);
-                q.pop();
-            }
-        }
-        return arr;
+        vector<int> ans;
+        return rightView(ans,root,1);
         
+        
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // vector<int> arr;
+        // TreeNode* temp;
+        // int size;
+        // while(!q.empty())
+        // {
+        //     size=q.size();
+        //     temp=q.front();
+        //     arr.push_back(temp->val);
+        //     for(int i=0;i<size;i++)
+        //     {
+        //         if(q.front()->right)
+        //             q.push(q.front()->right);
+        //         if(q.front()->left)
+        //             q.push(q.front()->left);
+        //         q.pop();
+        //     }
+        // }
+        // return arr;
     }
 };
