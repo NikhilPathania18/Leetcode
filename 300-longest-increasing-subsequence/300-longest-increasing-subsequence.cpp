781df7e17395ba9f6b3ce4a5580ac6aa1b58e686
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-07-11 02:17:38 +0530

    Time: 13 ms (84.54%), Space: 10.4 MB (67.34%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,7 +1,41 @@
 class Solution {
 public:
+    int getIndex(vector<int>& arr,int num)
+    {
+        int start=0,end=arr.size()-1;
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+            if(arr[mid]==num)   return mid;
+            if(num<arr[mid])    end=mid-1;
+            else    start=mid+1;
+        }
+        return end+1;
+    }
+    int binaryApproach(vector<int>& nums)
+    {
+        int n=nums.size();
+        vector<int> arr;
+        int size=0;
+        for(int i=0;i<n;i++)
+        {
+            if(size==0||nums[i]>arr[size-1])   {
+                arr.push_back(nums[i]);
+                size++;
+            }
+            else
+            {
+                int index=getIndex(arr,nums[i]);
+                arr[index]=nums[i];
+            }
+        }
+        return size;
+    }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
+        return binaryApproach(nums);
+        
+        
         vector<int> dp(n,1);
         int maxl=1;
         for(int i=0;i<n;i++)
