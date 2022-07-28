8edfbb5a24db10ce19c0c6130cac12251df07295
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-07-28 11:02:12 +0530

    Time: 16 ms (48.89%), Space: 7.4 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        unordered_map<char,int> m;
+        for(int i=0;i<s.size();i++)
+            m[s[i]]++;
+        
+        for(int i=0;i<t.size();i++)
+            m[t[i]]--;
+        
+        for(auto i: m)
+            if(i.second!=0) return false;
+        return true;
     }
 };
