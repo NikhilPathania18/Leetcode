2de2bc2621a4016f2a4b333fd4472d468052f633
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-08-17 23:17:28 +0530

    Time: 38 ms (61.51%), Space: 18 MB (39.53%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -8,15 +8,15 @@ public:
             ans.push_back(arr);
             return;
         }
-        if(index>n) return;
-        //take
+        if(index<=n)
+        {//take
         arr.push_back(index);
         solve(ans,arr,index+1,n,k);
         arr.pop_back();
         
         //notTake
         solve(ans,arr,index+1,n,k);
-        
+        }
     }
     vector<vector<int>> combine(int n, int k) {
         vector<int> arr;
