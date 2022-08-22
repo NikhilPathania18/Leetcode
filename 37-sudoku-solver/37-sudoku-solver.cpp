6ee992b6f26b9302b830348c9ec91a52ab1a9b7e
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-08-22 23:17:09 +0530

    Time: 45 ms (53.39%), Space: 6.2 MB (95.06%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,48 +1,37 @@
 class Solution {
 public:
-    bool checkRow(char num, vector<vector<char>> & board, int row, int col)
+    bool inRow(vector<vector<char>>& board,int row,int col,char num)
     {
         for(int i=0;i<9;i++)
         {
             if(board[row][i]==num)
-                return false;
+                return true;
         }
-        return true;
+        return false;
     }
-    bool checkCol(char num, vector<vector<char>> &board, int row, int col)
+    bool inCol(vector<vector<char>>& board,int row,int col,char num)
     {
         for(int i=0;i<9;i++)
         {
             if(board[i][col]==num)
-                return false;
+                return true;
         }
-        return true;
+        return false;
     }
-    bool checkBox(char num, vector<vector<char>> &board, int row, int col)
+    bool inBox(vector<vector<char>>& board,int row,int col,char num)
     {
-        int rowStart,colStart;
-        rowStart=row-row%3;
-        colStart=col-col%3;
+        int rowStart=row-row%3;
+        int colStart=col-col%3;
         for(int i=0;i<3;i++)
         {
             for(int j=0;j<3;j++)
             {
-                if(board[i+rowStart][j+colStart]==num)
-                    return false;
+                if(board[rowStart+i][colStart+j]==num)  return true;
             }
         }
-        return true;
-    }
-    
-    
-    bool canPlace(char num, vector<vector<char>> &board, int row, int col)
-    {
-        if(checkRow(num,board,row,col)&&checkCol(num,board,row,col)&&checkBox(num,board,row,col))
-            return true;
         return false;
     }
-    
-    bool solve(vector<vector<char>>& board,int row,int col)
+    bool checkSudoku(vector<vector<char>>& board,int row,int col)
     {
         if(col==9)
         {
@@ -53,22 +42,19 @@ public:
             return true;
         
         if(board[row][col]!='.')
-            return solve(board,row,col+1);
-        
-        
-        for(int i=1;i<=9;i++)
-        {
-            if(canPlace('0'+i,board,row,col))
+        return checkSudoku(board,row,col+1);
+            for(int i=1;i<=9;i++)
             {
-                board[row][col]='0'+i;
-                if(solve(board,row,col+1))
-                    return true;
-                board[row][col]='.';
+                if(!inRow(board,row,col,i+'0')&&!inCol(board,row,col,i+'0')&&!inBox(board,row,col,i+'0'))
+                {
+                    board[row][col]=i+'0';
+                    if(checkSudoku(board,row,col+1))    return true;
+                    board[row][col]='.';
+                }
             }
-        }
         return false;
     }
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board,0,0);
+        checkSudoku(board,0,0);
     }
 };
