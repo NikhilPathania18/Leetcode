fc565a04eeff128e27d7e432beef408d09f5909d
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2022-08-22 15:52:50 +0530

    Time: 9 ms (65.80%), Space: 7.2 MB (66.07%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,60 +1,30 @@
 class Solution {
 public:
-//     bool isSafe(int row,int col,vector<string> &board,int n,vector<bool> &ld,vector<bool> &rowD, vector<bool> &rd)
-//     {
-//         int x=row;
-//         int y=col;
-        
-//         //To check upper left diagonal
-//         while(x>=0&&y>=0)
-//         {
-//             if(board[x][y]=='Q')
-//                 return false;
-//             x--;
-//             y--;
-//         }
-        
-//         //To check back side of row
-//         x=row;
-//         y=col;
-        
-//         while(y>=0)
-//         {
-//             if(board[x][y]=='Q')
-//                 return false;
-//             y--;
-//         }
-        
-//         //To check lower diagonal
-//         x=row;
-//         y=col;
-//         while(y>=0&&x<n)
-//         {
-//             if(board[x][y]=='Q')
-//                 return false;
-//             x++;
-//             y--;
-//         }
-        
-//         return true;
-//     }
-    
-    
-    bool isSafe(int row,int col,vector<string> &board,int n,vector<bool> &ld,vector<bool> &rowD, vector<bool> &rd)
+    bool isSafe(int row,int col,vector<string>& board,int n)
     {
-        if(rd[row+col])
-            return false;
-        if(rowD[row])
-            return false;
+        int x=row,y=col;
+        while(y>=0)
+        {
+            if(board[x][y]=='Q')    return false;
+            y--;
+        }
         
-        //right diagonal formula= (n-1)+(col-row)
-        if(ld[(n-1)+(col-row)])
-            return false;
+        x=row;
+        y=col;
+        while(x>=0&&y>=0)
+        {
+            if(board[x--][y--]=='Q')    return false;
+        }
+        
+        x=row;
+        y=col;
+        while(x<n&&y>=0)
+            if(board[x++][y--]=='Q')    return false;
         
         return true;
     }
-        
-    void solve(int col,vector<string> &board,int n,vector<vector<string>>& ans,vector<bool>& leftDiagonal,vector<bool>& rowLine,vector<bool>& rightDiagonal)
+    
+    void solve(vector<vector<string>>& ans,vector<string>& board,int col,int n)
     {
         if(col==n)
         {
@@ -64,31 +34,21 @@ public:
         
         for(int i=0;i<n;i++)
         {
-            if(isSafe(i,col,board,n,leftDiagonal,rowLine,rightDiagonal))
+            if(isSafe(i,col,board,n))
             {
                 board[i][col]='Q';
-                rightDiagonal[i+col]=true;
-                rowLine[i]=true;
-                leftDiagonal[(n-1)+(col-i)]=true;
-                solve(col+1,board,n,ans,leftDiagonal,rowLine,rightDiagonal);
-                rightDiagonal[i+col]=false;
-                rowLine[i]=false;
-                leftDiagonal[(n-1)+(col-i)]=false;
+                solve(ans,board,col+1,n);
                 board[i][col]='.';
             }
         }
     }
-    
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        vector<string> board(n);
-        vector<bool> leftDiagonal(2*n-1,false);
-        vector<bool> row(n,false);
-        vector<bool> rightDiagonal(2*n-1,false);
         string s(n,'.');
+        vector<string> board;
         for(int i=0;i<n;i++)
-            board[i]=s;
-        solve(0,board,n,ans,leftDiagonal,row,rightDiagonal);
+            board.push_back(s);
+        solve(ans,board,0,n);
         return ans;
     }
 };
