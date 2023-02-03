2201a4c0d1e53e9da8f17e8f8880fb4110ec4120
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2023-02-03 22:44:55 +0530

    Time: 27 ms (39.67%), Space: 14.4 MB (23.99%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        vector<vector<char>> arr(numRows);
+        if(numRows==1)
+            return s;
+        int sPtr=0,arrPtr=0;
+        int flow=1;
+        for(int i=0;i<s.size();i++)
+        {
+              if(arrPtr==0)
+              {
+                  arr[arrPtr].push_back(s[i]);
+                  flow=1;
+                  arrPtr++;
+              }
+                else if(arrPtr==numRows-1)
+                {
+                    arr[arrPtr].push_back(s[i]);
+                    flow=0;
+                    arrPtr--;
+                }
+            else if(flow==1)
+            {
+                arr[arrPtr].push_back(s[i]);
+                arrPtr++;
+            }
+            else
+            {
+                arr[arrPtr].push_back(s[i]);
+                arrPtr--;
+            }
+        }
+        string str;
+        for(int i=0;i<arr.size();i++)
+        {
+            for(int j=0;j<arr[i].size();j++)
+            {
+                str.push_back(arr[i][j]);
+            }
+        }
+        return str;
+    }
+};
